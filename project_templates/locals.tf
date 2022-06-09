0d465ae3b46445be87c45aea90ee8de157a5e053
jmanzur <jmanzurst@gmail.com>
2022-06-08 23:05:16 -0300

    role fix

--- project_templates/locals.tf
+++ project_templates/locals.tf
@@ -1,6 +1,6 @@
 locals {
   roles = {
-    dev  = "arn:aws:iam::${var.aws_accounts["dev"]}:role/tf-codepipeline-deploy-role"
+    dev  = "arn:aws:iam::${var.aws_accounts["dev"]}:role/terraform-apply-role"
   }
   
   deploy_role = local.roles[terraform.workspace]
